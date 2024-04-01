b8946689fe9af9f02dbeb205fa33f509429da9a8
Dimas Knitto <143385412+dimasknitto@users.noreply.github.com>
2024-04-01 15:55:27 +0700

    Update excec-query.sql

--- excec-query.sql
+++ excec-query.sql
@@ -1,1 +1,25 @@
-DELETE FROM testing;
+CREATE PROCEDURE `hapusorder`(
+	 IN notransaksi VARCHAR(200),OUT out_msg VARCHAR(255))
+BEGIN
+
+DECLARE done INT DEFAULT 0;
+DECLARE sqlstate_code CHAR(5) DEFAULT '00000';
+DECLARE v_rollback BOOL DEFAULT 0; 
+
+
+DECLARE message_text TEXT;
+DECLARE mysql_errno INT;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done=1;
+DECLARE CONTINUE HANDLER FOR SQLEXCEPTION 
+	BEGIN		
+		GET DIAGNOSTICS CONDITION 1
+		sqlstate_code = RETURNED_SQLSTATE,
+		mysql_errno = MYSQL_ERRNO,
+		message_text = MESSAGE_TEXT;
+	
+		SET v_rollback = 1; 
+	END;
+
+  DELETE FROM testing;
+
+    END
